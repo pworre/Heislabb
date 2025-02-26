b6888d2b9198a6903826a49fb80229275bfee8bd
Hans Tomren <hanstomr@stud.ntnu.no>
2025-02-26 16:01:43 +0100

    initial commit

--- skeleton_project/source/driver/ElevatorControl.c
+++ skeleton_project/source/driver/ElevatorControl.c
@@ -1,1 +1,16 @@
 #include "elevatorControl.h"
+
+ElevatorState ctrl_getElevatorState(Elevator* anElevator) {
+    return anElevator->state;
+}
+void ctrl_updateElevatorState(Elevator* anElevator, ElevatorState aState){
+    anElevator->state = aState;
+}
+
+void ctrl_startup(Elevator* anElevator) {
+    elevio_motorDirection(DIRN_DOWN);
+    if (elevio_floorSensor >= 0) {
+        elevio_motorDirection(DIRN_STOP);
+        ctrl_updateElevatorState(anElevator, STATIONARY);
+    }
+}
--- skeleton_project/source/driver/ElevatorControl.h
+++ skeleton_project/source/driver/ElevatorControl.h
@@ -1,9 +1,13 @@
 #pragma once
 
 #include "elevatorStateMachine.h"
+#include "elevio.h"
 
 struct Elevator {
-    ElevatorState state = MOVING_DOWN;
+    ElevatorState state;
 };
 
-ElevatorState ctrl_getElevatorState();
+ElevatorState ctrl_getElevatorState(Elevator* anElevator);
+void ctrl_updateElevatorState(Elevator* anElevator, ElevatorState aState);
+
+void ctrl_startup(Elevator* anElevator);

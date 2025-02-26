264438f87ac925d4a5749ed10313c4a5208bfecc
Hans Tomren <hanstomr@stud.ntnu.no>
2025-02-26 16:22:34 +0100

    Added startup function and stuff

--- skeleton_project/source/driver/ElevatorControl.c
+++ skeleton_project/source/driver/ElevatorControl.c
@@ -7,10 +7,15 @@ void ctrl_updateElevatorState(Elevator* anElevator, ElevatorState aState){
     anElevator->state = aState;
 }
 
+void ctrl_setFloor(Elevator* anElevator, int floor) {
+    anElevator->lastFloor = floor;
+}
+
 void ctrl_startup(Elevator* anElevator) {
     elevio_motorDirection(DIRN_DOWN);
     if (elevio_floorSensor >= 0) {
         elevio_motorDirection(DIRN_STOP);
         ctrl_updateElevatorState(anElevator, STATIONARY);
+        ctrl_setFloor(anElevator, elevio_floorsensor());
     }
 }
--- skeleton_project/source/driver/ElevatorControl.h
+++ skeleton_project/source/driver/ElevatorControl.h
@@ -5,9 +5,11 @@
 
 struct Elevator {
     ElevatorState state;
+    int lastFloor;
 };
 
 ElevatorState ctrl_getElevatorState(Elevator* anElevator);
 void ctrl_updateElevatorState(Elevator* anElevator, ElevatorState aState);
+void ctrl_setFloor(Elevator* anElevator, int floor);
 
 void ctrl_startup(Elevator* anElevator);

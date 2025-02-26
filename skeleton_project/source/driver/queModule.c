81e623c04bb70a297825dac9746f8492886d1e5e
Hans Tomren <hanstomr@stud.ntnu.no>
2025-02-26 16:49:19 +0100

    Me fortsetter

--- skeleton_project/source/driver/queModule.c
+++ skeleton_project/source/driver/queModule.c
@@ -1,2 +1,11 @@
 #include "queModule.h"
 
+void que_checkQue(struct Elevator* anElevator, struct Orders* que) {
+    if ((que->order < anElevator->lastFloor) && (anElevator->state != MOVING_UP)) {
+        elevio_motorDirection(DIRN_DOWN);
+        ctrl_updateElevatorState(anElevator, MOVING_DOWN);
+    } else if ((que->order > anElevator->lastFloor) && (anElevator->state != MOVING_DOWN)) {
+        elevio_motorDirection(DIRN_UP);
+        ctrl_updateElevatorState(anElevator, MOVING_DOWN);
+    }
+}
--- skeleton_project/source/driver/queModule.h
+++ skeleton_project/source/driver/queModule.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "elevatorControl.h"
+
+struct Orders {
+    int order;
+    struct Orders *next;
+};
+
+void que_checkQue(struct Elevator* anElevator, struct Orders* que);

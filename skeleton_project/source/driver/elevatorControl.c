ec801fd284513ae5844006adef905767aa80539d
hanstomr <hanstomr@stud.ntnu.no>
2025-03-11 11:07:02 +0100

    Teit test

--- skeleton_project/source/driver/elevatorControl.c
+++ skeleton_project/source/driver/elevatorControl.c
@@ -65,9 +65,16 @@ void ctrl_run(Elevator* anElevator) {
     anElevator->run = 1;
 
     while (anElevator->run) {
+
+        printf("Before update: LastFloor: %d, NextOutsideOrder: %d, NextCabOrder: %d\n",
+            anElevator->lastFloor, anElevator->nextOutsideOrder_floor, anElevator->nextCabOrder);
+
         if ((anElevator != NULL) && (orderHead != NULL) && (cabOrderHead != NULL)) {
             SM_updateElevatorState(anElevator, orderHead, cabOrderHead);
         }
+
+        printf("After update: LastFloor: %d, NextOutsideOrder: %d, NextCabOrder: %d, NextFloor: %d\n",
+            anElevator->lastFloor, anElevator->nextOutsideOrder_floor, anElevator->nextCabOrder, anElevator->nextFloor);
 
         que_printOrders(orderHead);
         que_printCabOrders(cabOrderHead);

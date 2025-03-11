4d72cc8721e02fb86e67111bdbe56509a5035d71
hanstomr <hanstomr@stud.ntnu.no>
2025-03-11 11:11:24 +0100

    Teit test

--- skeleton_project/source/driver/elevatorControl.c
+++ skeleton_project/source/driver/elevatorControl.c
@@ -71,7 +71,7 @@ void ctrl_run(Elevator* anElevator) {
 
         if ((anElevator != NULL) && (orderHead != NULL) && (cabOrderHead != NULL)) {
             SM_updateElevatorState(anElevator, orderHead, cabOrderHead);
-            printf("SM har blitt oppdatert!\n")
+            printf("SM har blitt oppdatert!\n");
         }
 
         printf("After update: LastFloor: %d, NextOutsideOrder: %d, NextCabOrder: %d, NextFloor: %d\n",
